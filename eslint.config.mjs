44285db81f18d513794784e3d321d114f93f8085
Vitaliy <vlasov.v@sibtrans.ru>
2025-04-22 02:12:17 +0700

    fix

--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-// @ts-check
-import withNuxt from '#build/eslint.config.mjs'
-
-export default withNuxt(
-  // Your custom configs here
-)
--- pages/index.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <ContentRenderer v-if="false" />
-  <div v-else>Home not found</div>
-</template>
